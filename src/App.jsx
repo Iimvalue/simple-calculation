d1db4c37a23a45e31a2c72d09cc3ffdc241a5a4b
Abdulrahman <1xvlux1@gmail.com>
2025-05-25 10:54:15 +0300

    Calc App

---
+++ src/App.jsx
@@ -0,0 +1,156 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [secondNumber, setSecondNumber] = useState(0);
+  const [result, setResult] = useState(0);
+
+  function handleAddition() {
+    setResult(firstNumber + secondNumber);
+  }
+
+  function handleSubtraction() {
+    setResult(firstNumber - secondNumber);
+  }
+
+  function handleMultiplication() {
+    setResult(firstNumber * secondNumber);
+  }
+
+  function handleDivision() {
+    setResult(firstNumber / secondNumber);
+  }
+
+  return (
+    <>
+      {/* <div className="flex flex-col ">
+        <input type="text" onChange={(e) => setFirstNumber(e.target.value)} />
+        <input type="text" onChange={(e) => setSecondNumber(e.target.value)} />
+
+        <button onClick={handleAddition}>+</button>
+        <button onClick={handleSubtraction}>-</button>
+        <button onClick={handleMultiplication}>*</button>
+        <button onClick={handleDivision}>/</button>
+
+        <h1>The Result is: {result}</h1>
+      </div> */}
+      <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="font-bold text-4xl mb-10">Simple Calculations</h1>
+        <div>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="inline-full-name"
+              >
+                First Number
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                id="inline-full-name"
+                type="text"
+                placeholder="First Number"
+                onChange={(e) => {
+                  setFirstNumber(Number(e.target.value));
+                }}
+              />
+            </div>
+          </div>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                Second Number
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                type="text"
+                placeholder="Second Number"
+                onChange={(e) => {
+                  setSecondNumber(Number(e.target.value));
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                Result
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                type="text"
+                disabled
+                value={result}
+                placeholder={"Result"}
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-end w-80 gap-4">
+            <div className="md:flex md:items-center">
+              <div className="md:w-1/3" />
+              <div className="md:w-2/3">
+                <button
+                  className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                  type="button"
+                  onClick={handleAddition}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+
+            <div className="md:flex md:items-center">
+              <div className="md:w-1/3" />
+              <div className="md:w-2/3">
+                <button
+                  className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                  type="button"
+                  onClick={handleSubtraction}
+                >
+                  -
+                </button>
+              </div>
+            </div>
+
+            <div className="md:flex md:items-center">
+              <div className="md:w-1/3" />
+              <div className="md:w-2/3">
+                <button
+                  className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                  type="button"
+                  onClick={handleMultiplication}
+                >
+                  *
+                </button>
+              </div>
+            </div>
+
+            <div className="md:flex md:items-center">
+              <div className="md:w-1/3" />
+              <div className="md:w-2/3">
+                <button
+                  className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                  type="button"
+                  onClick={handleDivision}
+                >
+                  /
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
